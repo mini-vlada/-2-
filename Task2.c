6bef187b7ca8df574fa601c6e7cbdc3fd873a2b7
mini-vlada <113668977+mini-vlada@users.noreply.github.com>
2022-11-30 15:32:25 +0300

    Update Task2.c

--- Task2.c
+++ Task2.c
@@ -1,25 +1,14 @@
-
-
 #include <stdio.h>
 
 #include <stdlib.h>
 
-int main(void)
-
-{
-
-int magic; /* Волшебное число */
-
-int guess; /* Предположение игрока */
-
-magic = rand(); /* Генерируем волшебное число */
-
-printf("Угадай волшебное число: ");
-scanf(“%d", &guess);
-
-if(guess == magic) printf("** Верно **"); 
-else printf("Неверно");
-
-return 0;
-
-}
+int main(void) {
+    int magic; /* Волшебное число */
+    int guess; /* Предположение игрока */
+    magic = rand(); /* Генерируем волшебное число */
+    printf("Угадай волшебное число: ");
+    scanf(“ % d ", &guess);
+      if (guess == magic) printf("** Верно **");
+      else printf("Неверно");
+      return 0;
+    }
